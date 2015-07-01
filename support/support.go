3b110fd17462bb8df0230bcc6b6e5aed31f48f91
Graham Smith <neuegram@gmail.com>
2015-07-01 10:29:12 -0500

    Added CheckSizeInRange function

--- support/support.go
+++ support/support.go
@@ -12,3 +12,9 @@ func CheckSize(buf []byte, expected int, descrip string) {
 		panic(fmt.Sprintf("Incorrect %s buffer size, expected (%d), got (%d).", descrip, expected, len(buf)))
 	}
 }
+
+func CheckSizeInRange(size int, min int, max int, descrip string) {
+	if size < min || size > max {
+		panic(fmt.Sprintf("Incorrect %s buffer size, expected (%d - %d), got (%d).", descrip, min, max, size))
+	}
+}
