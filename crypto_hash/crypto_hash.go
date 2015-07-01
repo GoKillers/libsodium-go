30bd506ee220fe2f687d4e766280c7ce49cc7467
Graham Smith <neuegram@gmail.com>
2015-06-30 19:24:35 -0500

    Added crypto_hash.go

---
+++ crypto_hash/crypto_hash.go
@@ -0,0 +1,25 @@
+package cryptohash
+
+// #cgo pkg-config: libsodium
+// #include <stdlib.h>
+// #include <sodium.h>
+import "C"
+import "github.com/GoKillers/libsodium-go/support"
+
+func CryptoHashBytes() int {
+	return int(C.crypto_hash_bytes())
+}
+
+func CryptoHashPrimitive() string {
+	return C.GoString(C.crypto_hash_primitive())
+}
+
+func CryptoHash(in []byte) ([]byte, int) {
+	out := make([]byte, CryptoHashBytes())
+	exit := int(C.crypto_hash(
+		(*C.uchar)(&out[0]),
+		(*C.uchar)(&in[0]),
+		(C.ulonglong)(len(in))))
+
+	return out, exit
+}
