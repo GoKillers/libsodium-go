d980dc898553254f25a94f4ff04a5cbb4e6e46b3
Graham Smith <neuegram@gmail.com>
2015-11-02 20:40:20 -0600

    Added support for crypto_aead_aes256gcm_encrypt and crypto_aead_aes256gcm_decrypt, including defined bytes for size checks (available since libsodium v1.0.4)

---
+++ cryptoaead/crypto_aead_aes256gcm.go
@@ -0,0 +1,67 @@
+package cryptoaead
+
+// #cgo pkg-config: libsodium
+// #include <stdlib.h>
+// #include <sodium.h>
+import "C"
+import "github.com/GoKillers/libsodium-go/support"
+
+func CryptoAEADAES256GCMKeyBytes() int {
+	return int(C.crypto_aead_aes256gcm_keybytes())
+}
+
+func CryptoAEADAES256GCMNSecBytes() int {
+	return int(C.crypto_aead_aes256gcm_keybytes())
+}
+
+func CryptoAEADAES256GCMNPubBytes() int {
+	return int(C.crypto_aead_aes256gcm_npubbytes())
+}
+
+func CryptoAEADAES256GCMABytes() int {
+	return int(C.crypto_aead_aes256gcm_abytes())
+}
+
+func CryptoAEADAES256GCMStateBytes() int {
+	return int(C.crypto_aead_aes256gcm_statebytes())
+}
+
+func CryptoAESAES256GCMIsAvailable() int {
+	return int(C.crypto_aes_aes256gcm_is_available())
+}
+
+func CryptoAEADAES256GCMEncrypt(m []byte, ad []byte, nsec []byte, npub []byte, k []byte) ([]byte, int) {
+	support.CheckSize(k, CryptoAEADAES256GCMKeyBytes(), "secret key")
+	support.CheckSize(npub, CryptoAEADAES256GCMNPubBytes(), "public nonce")
+	c := make([]byte, len(m)+CryptoAEADAES256GCMABytes())
+	exit := int(C.crypto_aead_aes256gcm_encrypt(
+		(*C.uchar)(&c[0]),
+		(C.ulonglong)(len(c)),
+		(*C.uchar)(&m[0]),
+		(C.ulonglong)(len(m)),
+		(*C.uchar)(&ad[0]),
+		(C.ulonglong)(len(ad)),
+		(*C.uchar)(&nsec[0]),
+		(*C.uchar)(&npub[0]),
+		(*C.uchar)(&k[0])))
+
+	return c, exit
+}
+
+func CryptoAEADAES256GCMDecrypt(nsec []byte, c []byte, ad []byte, npub []byte, k []byte) ([]byte, int) {
+	support.CheckSize(k, CryptoAEADAES256GCMKeyBytes(), "secret key")
+	support.CheckSize(npub, CryptoAEADAES256GCMNPubBytes(), "public nonce")
+	m := make([]byte, len(c)-CryptoAEADAES256GCMABytes())
+	exit := int(C.crypto_aead_aes256gcm_decrypt(
+		(*C.uchar)(&m[0]),
+		(C.ulonglong)(len(m)),
+		(*C.uchar)(&nsec[0]),
+		(*C.uchar)(&c[0]),
+		(C.ulonglong)(len(c)),
+		(*C.uchar)(&ad[0]),
+		(C.ulonglong)(len(ad)),
+		(*C.uchar)(&npub[0]),
+		(*C.uchar)(&k[0])))
+
+	return m, exit
+}
