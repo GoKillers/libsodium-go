bef46514f042cff70851b1d5e7a06fd47c60bedd
Stephen Chavez <stephen@dicesoft.net>
2016-02-13 15:43:59 -0700

    Update argon2.go

--- cryptopwhash/argon2.go
+++ cryptopwhash/argon2.go
@@ -4,7 +4,7 @@ package cryptopwhash
 // #include <stdlib.h>
 // #include <sodium.h>
 import "C"
-import "github.com/GoKillers/libsodium-go/support"
+_ import "github.com/GoKillers/libsodium-go/support"
 
 func Argon2iHashRaw(tCost int,
 					mCost int,
