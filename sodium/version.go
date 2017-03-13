af534f22a543413e038ef85a4963af1efcb6fb3a
Silke <silke@slxh.eu>
2017-03-13 21:23:53 +0100

    Wrap sodium/version

---
+++ sodium/version.go
@@ -0,0 +1,18 @@
+package sodium
+
+// #cgo pkg-config: libsodium
+// #include <stdlib.h>
+// #include <sodium.h>
+import "C"
+
+func SodiumVersionString() string {
+	return C.GoString(C.sodium_version_string())
+}
+
+func SodiumLibaryVersionMajor() int {
+	return int(C.sodium_library_version_major())
+}
+
+func SodiumLibaryVersionMinor() int {
+	return int(C.sodium_library_version_minor())
+}
