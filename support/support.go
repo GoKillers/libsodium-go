9ee29882ca740f41161d52aed6071df6aa1d92f2
Silke <silke@slxh.eu>
2017-04-28 20:13:37 +0200

    Add errors and more support functions

--- support/support.go
+++ support/support.go
@@ -1,30 +1,46 @@
+// Package support implements support functions and errors that are used by by other libsodium-go packages.
 package support
 
 import "fmt"
 
-//
-// Internal support functions
-//
-
-// CheckSize verifies the expected size of an input or output byte array.
+// CheckSize checks if the length of a byte slice is equal to the expected length,
+// and panics when this is not the case.
 func CheckSize(buf []byte, expected int, descrip string) {
 	if len(buf) != expected {
 		panic(fmt.Sprintf("Incorrect %s buffer size, expected (%d), got (%d).", descrip, expected, len(buf)))
 	}
 }
 
+// CheckSizeMin checks if the length of a byte slice is greater or equal than a minimum length,
+// and panics when this is not the case.
 func CheckSizeMin(buf []byte, min int, descrip string) {
 	if len(buf) < min {
 		panic(fmt.Sprintf("Incorrect %s buffer size, expected (>%d), got (%d).", descrip, min, len(buf)))
 	}
 }
 
-func CheckSizeInRange(size int, min int, max int, descrip string) {
-	if size < min || size > max {
-		panic(fmt.Sprintf("Incorrect %s buffer size, expected (%d - %d), got (%d).", descrip, min, max, size))
+// CheckSizeInRange checks if the length of a byte slice is between a lower and upper boundaries.
+func CheckSizeInRange(buf []byte, min int, max int, descrip string) {
+	if len(buf) < min || len(buf) > max {
+		panic(fmt.Sprintf("Incorrect %s buffer size, expected (%d - %d), got (%d).", descrip, min, max, len(buf)))
 	}
 }
 
+// CheckSizeGreaterOrEqual checks if the length of a byte slice is greater or equal to that of a second byte slice.
+func CheckSizeGreaterOrEqual(a, b []byte, aDescription, bDescription string) {
+	if len(a) < len(b) {
+		panic(fmt.Sprintf("%s smaller than %s", aDescription, bDescription))
+	}
+}
+
+// NilPanic is a shorthand that results in a panic when called with true.
+func NilPanic(t bool, description string) {
+	if t {
+		panic(description + " is a nil pointer")
+	}
+}
+
+// BytePointer returns a pointer to the start of a byte slice, or nil when the slice is empty.
 func BytePointer(b []byte) *uint8 {
 	if len(b) > 0 {
 		return &b[0]
